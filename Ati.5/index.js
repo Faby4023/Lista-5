4400e3344efa04f22ea2811eee51dd0a554e07ee
Faby4023 <143666861+Faby4023@users.noreply.github.com>
2024-08-05 10:42:00 -0300

    Add files via upload

---
+++ Ati.5/index.js
@@ -0,0 +1,130 @@
+//Questão 1
+//Permite que um trecho de código seja repetido por determinada vezes
+
+//Questão 2
+//Através de uma variável usada para controlar a quantidade de repetições
+
+//Questão 3
+//Quano a condição for falsa, o programa finaliza a repetição
+
+//Questão 4 
+//Através do icremento i++ que acrescenta mais 1 a cada repetição
+
+//Questão 5
+//let frase = prompt("Digite uma frase: !");
+//for(let i = 0; i > 10; i++){
+//console.log("Testando uma frase!")
+//}
+
+//Questão 6
+//let numeros = parseInt(prompt("Digite 10 numeros inteiros: !"));
+//for(let i = 0; i > 10; i++){
+//console.log(i)
+//}
+
+//Questão 7
+//let numeros = parseInt(prompt("Digite 10 numeros inteiros: !"));
+//for(let i = 0; i > 10; i++){
+//if(i ==5){
+//break;
+//}
+//console.log(i)
+//}
+
+//Questão 8
+//let numeros = parseInt(prompt("Digite 10 numeros inteiros: !"));
+//for(let i = 0; i > 10; i++){
+//if(i ==5){
+//continue;
+//}
+//console.log(i)
+//}
+//Questão 9
+// let nomes = ["Fabyanna","Pedro","Alisson","Matheus","Flávio"];
+// for(let n of nomes);
+// console.log(n)
+
+//Questão 10
+//let numeros = parseInt(prompt("Digite os numeros em ordem crescente"));
+//for(let i = 0; i <= 10, i++){
+//console.log(i)
+//}
+//Questão 11
+//let numeros = parseInt(prompt("Digite os numeros em ordem crescente"));
+//for(let i = 10; i <= 1, i--){
+//console.log(i)
+//}
+//Questão 12
+//let soma = 0;
+//for(let i = 1; i <= 100; i++) {
+// soma += i; 
+//    })
+//
+//const resultado = calcularSoma();
+//console.log(`A soma dos números de 1 a 100 é ${resultado}`);
+
+//Questão 13
+//function exibirPares(): void {
+    // Loop for para iterar de 1 a 50
+//for (let i = 1; i <= 50; i++) {
+    // Verifica se o número é par
+//if (i % 2 === 0) {
+//console.log(i); // Exibe o número par no console
+        //}
+    //}
+//}
+//Questão 14
+//function calcularProduto(): number {
+//let produto = 1; 
+   //Loop for para iterar de 1 a 5
+//for (let i = 1; i <= 5; i++) {
+//produto *= i; // Multiplica o valor de produto pelo valor atual de i
+//}
+//
+//const resultado = calcularProduto();
+//console.log(`O produto dos números de 1 a 5 é ${resultado}`)
+
+//Questão 15
+//function exibirTabuada(n: number): void {
+//console.log(`Tabuada do ${n}:`);
+    
+    // Loop for para iterar de 1 a 10
+//for (let i = 1; i <= 10; i++) {
+//const resultado = n * i; // Calcula o produto
+//console.log(`${n} x ${i} = ${resultado}`); // Exibe a multiplicação e o resultado
+//}
+//}
+//Questão 19
+//function exibirImpares(): void {
+    // Loop for para iterar de 1 a 100
+//for (let i = 1; i <= 100; i++) {
+//if (i % 2 !== 0) {
+//console.log(i); // Exibe o número ímpar no console
+//  }
+// }
+//}
+//Questão 22
+// function exibirDivisores(numero: number): void {
+//     console.log(`Divisores de ${numero}:`);
+//     for (let i = 1; i <= numero; i++) {
+//         if (numero % i === 0) {
+//             console.log(i); // Exibe o divisor no console
+//         }
+//     }
+// }
+
+// // Função principal para ler a entrada do usuário
+// function main(): void {
+//     rl.question('Digite um número inteiro: ', (entrada: string) => {
+//         const numero = parseInt(entrada, 10);
+
+//         if (isNaN(numero) || numero <= 0) {
+//             console.log('Por favor, digite um número inteiro positivo.');
+//         } else {
+//             exibirDivisores(numero);
+//        }
+//Questão 
+
+
+
+
